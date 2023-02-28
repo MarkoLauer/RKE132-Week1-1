06f1058c97fe3f39b2ef7a6479cac15063abcc72
MarkoLauer <marko.lauer@tktk.ee>
2023-02-28 17:55:20 +0200

    create new app

---
+++ NewTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// this is a comment
+
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+
+string UserName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello " + UserName + "!"); //output
+//string interpolation
+
+Console.WriteLine($"Hello, {UserName} !"); //output
+
